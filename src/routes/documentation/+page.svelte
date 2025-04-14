59ccd408d6ee5e77851b167ecd3d74aa9043d2ba
s023322 <s023322@students.lmsd.org>
2025-04-14 13:01:50 -0400

    Updated links in documentation

--- src/routes/documentation/+page.svelte
+++ src/routes/documentation/+page.svelte
@@ -34,12 +34,12 @@
     <h1 class="font-display text-2xl text-black relative mt-16 mb-8">Tools & Frameworks</h1>
 
     <ul class="list-none text-black mb-16">
-        <li><span class="font-semibold font-display">Deployment</span> - Github & Vercel</li>
-        <li><span class="font-semibold font-display">Environment</span> - NodeJS</li>
-        <li><span class="font-semibold font-display">Frontend</span> - Vite</li>
-        <li><span class="font-semibold font-display">Payment</span> - Stripe & Svelte-Stripe</li>
-        <li><span class="font-semibold font-display">Scripting</span> - Typescript & Javascript</li>
-        <li><span class="font-semibold font-display">Styling</span> - TailwindCSS</li>
-        <li><span class="font-semibold font-display">UI</span> - Svelte & Sveltekit</li>
+        <li><span class="font-semibold font-display">Deployment</span> - <a href="https://github.com">Github</a> & <a href="https://vercel.com">Vercel</a></li>
+        <li><span class="font-semibold font-display">Environment</span> - <a href="https://nodejs.org">NodeJS</a></li>
+        <li><span class="font-semibold font-display">Frontend</span> - <a href="https://vite.dev">Vite</a></li>
+        <li><span class="font-semibold font-display">Payment</span> - <a href="https://stripe.com">Stripe</a> & <a href="https://sveltestripe.com">Svelte-Stripe</a></li>
+        <li><span class="font-semibold font-display">Scripting</span> - <a href="https://www.typescriptlang.org/">Typescript</a> & <a href="https://www.javascript.com/">Javascript</a></li>
+        <li><span class="font-semibold font-display">Styling</span> - <a href="https://tailwindcss.com">TailwindCSS</a></li>
+        <li><span class="font-semibold font-display">UI</span> - <a href="https://svelte.dev">Svelte</a> & <a href="https://svelte.dev/docs/kit/introduction">Sveltekit</a></li>
     </ul>
 </div>
